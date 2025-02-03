3dd32a6944bdd98514bc98bd32c7a0ca5578713e
paaket <122210350+paaket@users.noreply.github.com>
2025-02-03 20:03:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-from dataclasses import dataclass
+# from dataclasses import dataclass
 
-from environs import Env
-
-
-@dataclass
-class TgBot:
-    token: str  # Токен для доступа к телеграм-боту
+# from environs import Env
 
 
-@dataclass
-class Config:
-    tg_bot: TgBot
+# @dataclass
+# class TgBot:
+#     token: str  # Токен для доступа к телеграм-боту
 
 
-# Создаем функцию, которая будет читать файл .env и возвращать экземпляр
-# класса Config с заполненными полями token и admin_ids
-def load_config(path: str | None = None) -> Config:
-    env = Env()
-    env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+# @dataclass
+# class Config:
+#     tg_bot: TgBot
+
+
+# # Создаем функцию, которая будет читать файл .env и возвращать экземпляр
+# # класса Config с заполненными полями token и admin_ids
+# def load_config(path: str | None = None) -> Config:
+#     env = Env()
+#     env.read_env(path)
+#     return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
